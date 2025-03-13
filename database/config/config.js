d9049c1bf7466577cf684bc0c3726fef808e9b0b
Vikkkkk <vicguo.99@gmail.com>
2025-03-12 18:08:10 -0700

    added redux in frontend

--- database/config/config.js
+++ database/config/config.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'postgres',
+    username: process.env.DB_USER || 'victor',
     password: process.env.DB_PASSWORD || 'password',
     database: process.env.DB_NAME || 'hospital_erp',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USER || 'postgres',
+    username: process.env.DB_USER || 'victor',
     password: process.env.DB_PASSWORD || 'password',
     database: process.env.DB_NAME || 'hospital_erp_test',
     host: process.env.DB_HOST || '127.0.0.1',
